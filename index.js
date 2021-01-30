3797c4838c10dc6d36a35162dc2bcf0a99e815d8
Tommaso Siligardi <tomesse@outlook.it>
2021-01-30 15:09:21 +0100

    commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const sqlite3 = require("sqlite3")
+const express = require ("express")
+const app = new express()
+app.use(express.json())
+
+
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(()=> {
+  db.run("CREATE TABLE users (username TEXT, password TEXT)");
+
+  var stmt = db.prepare("INSERT INTO users VALUES (?,?)")
+  for (var i = 0; i < 10; i++) {
+      stmt.run("pippo" + i, "pluto"+i)
+  }
+  stmt.finalize()
+})
+
+app.post("/login",(req,res)=>{
+    const { user,password } = req.body
+    console.log(req.body)
+    db.get("SELECT * FROM users WHERE username = ? AND password = ?",user,password,(err,row)=>{
+        if (err) console.log(err)
+        else{
+            if (row) res.json({status : 200, msg: "login"})
+            else res.json({status : 401, msg: "error"})
+        }
+    })
+})
+
+app.listen(8080,()=>{console.log('server listening on port 8080')})
